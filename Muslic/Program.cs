6d1ac50e8a68a4c09bd6f92a5bb9719ea9d726b3
crocovert <patrick.palmier@cerema.fr>
2025-08-18 17:36:44 +0200

    1.05.3.b

--- Muslic/Program.cs
+++ Muslic/Program.cs
@@ -1845,7 +1845,7 @@ namespace Muslic
                                     while (gga_nq[id_bucket].Count == 0)
                                     {
                                         id_bucket++;
-                                        if (id_bucket == gga_nq.Count || id_bucket == bucket_cout_max )
+                                        if (id_bucket == gga_nq.Count+1 || id_bucket == bucket_cout_max )
                                         {
                                             goto fin_gga;
                                         }
@@ -3030,7 +3030,7 @@ namespace Muslic
                                     while (gga_nq[id_bucket].Count == 0)
                                     {
                                         id_bucket++;
-                                        if (id_bucket == gga_nq.Count  || bucket_cout_max  == id_bucket)
+                                        if (id_bucket == gga_nq.Count+1  || bucket_cout_max  == id_bucket)
                                         {
                                             goto fin_gga2;
                                         }
